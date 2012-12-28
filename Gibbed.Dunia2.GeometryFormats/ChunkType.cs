517070b2264884e74799fbb7e071265cd469f3b2
Rick Gibbed <gibbed@users.noreply.github.com>
2012-12-28 21:57:03 +0000

    Corrected ChunkType definition.

--- Gibbed.Dunia2.GeometryFormats/ChunkType.cs
+++ Gibbed.Dunia2.GeometryFormats/ChunkType.cs
@@ -1,7 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/* Copyright (c) 2012 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
 
 namespace Gibbed.Dunia2.GeometryFormats
 {
@@ -24,8 +41,9 @@ namespace Gibbed.Dunia2.GeometryFormats
         UCMP = 0x55434D50,
         MaterialDescriptor = 0x444D544C, // 'DMTL'
 
-        //O2BM = 0x4F32424D,
-        //IKDA = 0x494B4441,
+        O2BM = 0x4F32424D, // 'O2BM'
+        IKDA = 0x494B4441, // 'IKDA'
+
         // ReSharper restore InconsistentNaming
     }
 }
